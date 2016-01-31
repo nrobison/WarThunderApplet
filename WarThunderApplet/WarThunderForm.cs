d58dbdfceaf65b661d29eebbd8626ecb5d29edfb
Nathaniel Robison <nate.robison@iinteractive.com>
2016-01-31 10:58:05 -0800

    Initial Commit

---
+++ WarThunderApplet/WarThunderForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarThunderApplet
+{
+    public partial class WarThunderForm : Form
+    {
+        public WarThunderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WarThunderForm_Load(object sender, EventArgs e)
+        {
+            var connection = LogitechGSDK.LogiLcdInit("War Thunder", LogitechGSDK.LOGI_LCD_TYPE_COLOR);
+            if (connection)
+            {
+                
+            }
+        }
+
+        private bool IsConnectedToWarThunder()
+        {
+            return false;
+        }
+
+        private void GetMap()
+        {
+            
+        }
+    }
+}
